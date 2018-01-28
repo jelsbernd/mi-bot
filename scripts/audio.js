6a0a7540a886a3c3052748c364395dd48f6bdfd5
jelsbernd <jelsbernd@hotmail.com>
2018-01-27 21:34:12 -0600

    hard-code the duration, comment out that Promise

--- scripts/audio.js
+++ scripts/audio.js
@@ -19,26 +19,39 @@ var message = "I'm speechless";
 var soundfile = "./sounds/breath.mp3";
 var track_dir = "./sounds/"
 var player = "mpg123 ";
-var mp3Duration = require('mp3-duration');
+// var mp3Duration = require('mp3-duration');
 var wink_len = 0;
 var dur = 0;
 var mp3dur = 0;
 var blinkstick = require('blinkstick');
 device = blinkstick.findFirst();
-
+// var wait = ms => new Promise((r, j) => setTimeout(r, ms))
 
 // lights Blinkstick physical device from system hosting hubot
-function eye_flash(track) {
+function eye_flash(dur, track) {
     if (eyes_on) {
-        dur = 2.88;
+        // var mp3Duration = require('mp3-duration');
+
+        // mp3Duration(track, function (err, duration) {
+        //     if (err) return console.log(err.message);
+        //     // await wait(2000);
+        //     console.log('Your file is ' + duration + ' seconds long');
+        // })
+        // client.set('joesdur', 2.19);
+
+        // dur = 4
+        // dur = client.get('joesdur');
+        console.log('dur is' + dur + ' seconds long');
+        // dur = await(duration);
         mp3dur = 0;
         ms_time = 0;
 
-        mp3dur = dur * 1000;
+        mp3dur = dur * 2750;
         while (ms_time <= mp3dur) {
             console.log("mp3dur", mp3dur);
-            wink_len = 0;
-            while (wink_len <= mp3dur) {
+            wink_len1 = 0;
+            // flash those eyes
+            while (wink_len1 <= mp3dur) {
                 wink = Math.floor(Math.random() * 50) + 50;
                 // wink = 50;
                 device.setColor("#FF0000", { 'channel': 0, 'index': 0 });
@@ -49,10 +62,12 @@ function eye_flash(track) {
                 device.setColor("#FF0000", { 'channel': 0, 'index': 5 });
                 device.setColor("#FF0000", { 'channel': 0, 'index': 6 });
                 device.setColor("#FF0000", { 'channel': 0, 'index': 7 });
-                wink_len += wink;
+                wink_len1 += wink;
+                console.log("wink_len1 top", wink_len1);
             }
-            wink_len = 0;
-            while (wink_len <= mp3dur) {
+            wink_len2 = 0;
+            // darken those eyes
+            while (wink_len2 <= mp3dur) {
                 // wink = Math.floor(Math.random() * 250) + 50;
                 wink = 50;
                 device.setColor("#000000", { 'channel': 0, 'index': 0 });
@@ -63,9 +78,10 @@ function eye_flash(track) {
                 device.setColor("#000000", { 'channel': 0, 'index': 5 });
                 device.setColor("#000000", { 'channel': 0, 'index': 6 });
                 device.setColor("#000000", { 'channel': 0, 'index': 7 });
-                wink_len += wink;
+                wink_len2 += wink;
+                console.log("wink_len2 bottom", wink_len2);
             }
-            ms_time += 500;
+            ms_time += wink_len1;
         }
     }
 }
@@ -75,10 +91,10 @@ function shhhh() {
 }
 
 // plays mp3 files from physical device hosting hubot
-function say(track) {
+function say(man_dur, track) {
     if (sounds_on) {
         shell.exec(player + track_dir + track, { async: true, silent: true });
-        eye_flash(track_dir + track);
+        eye_flash(man_dur, track_dir + track);
     }
 
 }
@@ -104,91 +120,97 @@ module.exports = function (robot) {
     robot.hear(/vader eyes on/igm, function (msg) {
         eyes_on = true;
         chat_it(msg, "Here's looking at you");
-        say("as_wish.mp3");
+        say(1.83, "as_wish.mp3");
     });
 
     robot.hear(/vader eyes off/igm, function (msg) {
         eyes_on = false;
         chat_it(msg, "I'm resting my eyes");
-        say("as_wish.mp3");
+        // 1.83
+        say(1.83, "as_wish.mp3");
     });
 
     robot.hear(/vader speech on/igm, function (msg) {
         sounds_on = true;
         chat_it(msg, "making noise");
-        say("as_wish.mp3");
+        say(1.83, "as_wish.mp3");
     });
 
     robot.hear(/vader speech off/igm, function (msg) {
         sounds_on = false;
         chat_it(msg, "silent running");
-        say("as_wish.mp3");
+        say(1.83, "as_wish.mp3");
     });
 
     robot.hear(/vader comms on/igm, function (msg) {
         comms_on = true;
         chat_it(msg, "engage chatter");
-        say("as_wish.mp3");
+        say(1.83, "as_wish.mp3");
     });
 
     robot.hear(/vader comms off/igm, function (msg) {
         comms_on = false;
         chat_it(msg, "cutting out the chatter");
-        say("as_wish.mp3");
+        say(1.83, "as_wish.mp3");
     });
 
     robot.hear(/vader dont fail/igm, function (msg) {
-        say("dontfail.mp3");
+        say(1.83, "dontfail.mp3");
     });
 
     robot.hear(/vader build pass/igm, function (msg) {
         // chat_it(msg, "Impressive");
-        say("proud.mp3");
+        say(1.83, "proud.mp3");
     });
 
     robot.hear(/Git push detected/igm, function (msg) {
         //  chat_it(msg, "Real Good");
-        say("pushit.mp3");
+        say(1.83, "pushit.mp3");
     });
 
     robot.hear(/vader breath/igm, function (msg) {
         //  chat_it(msg, "Real Good");
-        say("breath.mp3");
+        say(1.83, "breath.mp3");
     });
     robot.hear(/vader disintigrate/igm, function (msg) {
         //  chat_it(msg, "Real Good");
-        say("dsntgrt.mp3");
+        say(1.83, "dsntgrt.mp3");
     });
     robot.hear(/vader all to easy/igm, function (msg) {
         //  chat_it(msg, "Real Good");
-        say("all2easy.mp3");
+        // 1.1
+
+        say(1.1, "all2easy.mp3");
     });
     robot.hear(/vader waiting/igm, function (msg) {
         //  chat_it(msg, "Real Good");
-        say("waiting.mp3");
+        // 2.5
+        say(2.5, "waiting.mp3");
     });
     robot.hear(/vader power/igm, function (msg) {
         //  chat_it(msg, "Real Good");
-        say("power.mp3");
+        // 4.0
+        say(4.0, "power.mp3");
     });
     robot.hear(/vader no escape/igm, function (msg) {
         //  chat_it(msg, "Real Good");
-        say("noescap.mp3");
+        // 5.0
+        say(5.0, "noescap.mp3");
     });
     robot.hear(/vader impressive/igm, function (msg) {
         //  chat_it(msg, "Real Good");
-        say("impresiv.mp3");
+        say(1.83, "impresiv.mp3");
     });
     robot.hear(/vader bidding/igm, function (msg) {
         //  chat_it(msg, "Real Good");
-        say("bidding.mp3");
+        say(1.83, "bidding.mp3");
     });
     robot.hear(/vader honored/igm, function (msg) {
         //  chat_it(msg, "Real Good");
-        say("honored.mp3");
+        say(1.83, "honored.mp3");
     });
     robot.hear(/vader force/igm, function (msg) {
         //  chat_it(msg, "Real Good");
-        say("theforce.mp3");
+        say(1.83, "theforce.mp3");
     });
 }
